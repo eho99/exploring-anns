b1d5c738035b5d1e8747049f2c7f23af12fb57c4
eho99 <ca.ericho@gmail.com>
2021-10-11 01:34:48 -0700

    added activation functions and weighted sum

--- src/Neuron.hpp
+++ src/Neuron.hpp
@@ -1,23 +1,52 @@
-#include <vector> 
+// https://towardsdatascience.com/step-by-step-the-math-behind-neural-networks-490dc1f3cfd9
+// https://takinginitiative.wordpress.com/2008/04/03/basic-neural-network-tutorial-theory/
+
+
+
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
-struct Connection {
+struct Connection
+{
     double weight, d_weight;
 };
 
 typedef vector<Neuron> Layer;
 
-class Neuron {
-    public:
-        Neuron(unsigned numOutputs);
+class Neuron
+{
+public:
+    Neuron(unsigned numOutputs);
 
-    private:
-        double m_outputVal;
-        vector<Connection> m_outputWeights;
-        static double randomWeight(void) {
-            return rand() / double(RAND_MAX);
-        }
+private:
+    vector<Connection> m_outputWeights;
+    static double randomWeight(void)
+    {
+        return rand() / double(RAND_MAX);
+    }
 };
 
 
+// Weighted Sum - Dot Product
+double weighted_sum(vector<double> weights, vector<double> inputs) {
+    double w_sum = 0;
+    for (int i = 0; i < sizeof(weights); ++i) {
+        w_sum += weights[i] * inputs[i];
+    }
+    return w_sum;
+}
+
+
+// Activation Functions
+
+double sigmoid(double x)
+{
+    return 1 / (1 + exp(-x));
+}
+
+double d_sigmoid(double x)
+{
+    return sigmoid(x) * (1 - sigmoid(x));
+}
--- src/network.hpp
+++ src/network.hpp
@@ -1,5 +1,5 @@
 #include <vector>
-#include <Neuron.hpp>
+#include "Neuron.hpp"
 
 using namespace std;
 
